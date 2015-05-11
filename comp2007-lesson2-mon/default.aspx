78e7fd3aa5927d74379f60858a84c93fa2ada616
ConnorX <connor_d_hall@hotmail.com>
2015-05-11 14:19:36 -0400

    first commit

---
+++ comp2007-lesson2-mon/default.aspx
@@ -0,0 +1,38 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="default.aspx.cs" Inherits="comp2007_lesson2_mon._default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Lesson 2 Homepage</title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        <asp:Label ID="lblFirstname" runat="server"></asp:Label>
+        <asp:Literal ID="ltlFirstname" runat="server"></asp:Literal>
+        <asp:TextBox ID="txtFirstname" runat="server"></asp:TextBox>
+        <asp:Button ID="btnSubmit" runat="server" Text="Click Me" OnClick="btnSubmit_Click" />
+        <asp:Label ID="lblX" runat="server" Visible="false"></asp:Label>
+
+    </div>
+    <div>
+        <asp:Literal ID="ltlToppings" runat="server" />
+        <asp:DropDownList runat="server" ID="ddlCountry">
+            <asp:ListItem Value="1" Text="Canada"></asp:ListItem>
+            <asp:ListItem Value="2" Text="Brazil"></asp:ListItem>
+            <asp:ListItem Value="3" Text="India"></asp:ListItem>
+        </asp:DropDownList>
+        <asp:CheckBoxList runat="server" ID="cblToppings">
+            <asp:ListItem Value="1" Text="Pepperoni"></asp:ListItem>
+            <asp:ListItem Value="2" Text="Bacon"></asp:ListItem>
+            <asp:ListItem Value="3" Text="Green Peppers"></asp:ListItem>
+        </asp:CheckBoxList>
+
+        <asp:Button runat="server" ID="btnSelections" Text="Show Selections" OnClick="btnSelections_Click" />
+
+    </div>
+
+    </form>
+</body>
+</html>
---
+++ comp2007-lesson2-mon/default.aspx.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace comp2007_lesson2_mon
+{
+    public partial class _default : System.Web.UI.Page
+    {
+        public int x;
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            //add code here
+            //only increment x if being reloaded
+            if(!IsPostBack)
+            {
+                x = 0;
+                x++;
+                lblX.Text = x.ToString();
+            }
+        }
+
+        protected void btnSubmit_Click(object sender, EventArgs e)
+        {
+            //get current count of x from label
+            x=Convert.ToInt32(lblX.Text);
+            x++;
+
+            //increment the lael value by 1 or write x back to the label
+            lblX.Text = x.ToString();
+
+            //copy value here
+            lblFirstname.Text = txtFirstname.Text;
+
+            //also copy to literal
+            ltlFirstname.Text = txtFirstname.Text;
+
+            //remove from textox
+            txtFirstname.Text = "";
+        }
+
+        protected void btnSelections_Click(object sender, EventArgs e)
+        {
+            //show the selected countries in the literal control
+            ltlFirstname.Text = ddlCountry.SelectedValue + " " + ddlCountry.SelectedItem.Text;
+
+            //first clear literal for toppings
+            ltlToppings.Text = "";
+            //loop through the toppings and print out selected toppings
+            foreach(ListItem item in cblToppings.Items)
+            {
+                if(item.Selected)
+                {
+                    ltlToppings.Text += item.Text + " ";
+                }
+            }
+        }
+    }
+}
